ba18268b29223393103d0e84f78ff0a301c43397
ppcodelearn <ppcodelearn@gmail.com>
2022-06-01 22:13:17 +0200

    change word 3

--- video.py
+++ video.py
@@ -74,8 +74,8 @@ else:
     )
 
 st.video(os.path.join(avdir, filename))
-# st.header("Remote video playback")
-# st.write("st.video allows a variety of HTML5 supported video links, including YouTube.")
+st.header("Remote video playback")
+st.write("st.video allows a variety of HTML5 supported video links, including YouTube.")
 
 
 def shorten_vid_option(opt):
